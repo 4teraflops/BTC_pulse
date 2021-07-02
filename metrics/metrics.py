1dc79a282b1457bb7340ca1d36702ba788fa891b
4teraflops <3lL05WzAr2UA>
2021-07-02 22:58:22 +0500

    - system metrics

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -9,25 +9,6 @@ logger.add(f'log/{__name__}.log', format='{time} {level} {message}', level='DEBU
 logger.debug('test')
 
 
-def get_cpu_procent():
-    #print(f'psutil.cpu_percent(): {psutil.cpu_percent()}')
-    return psutil.cpu_percent()
-
-
-def get_virtual_memory_procent_usage():
-    #print(f'psutil.virtual_memory().percent: {psutil.virtual_memory().percent}')
-    return psutil.virtual_memory().percent
-
-
-def get_disc_usage_percent():
-    #print(f'psutil.disk_usage.used.precent:{psutil.disk_usage("/").percent}')
-    return psutil.disk_usage("/").percent
-
-
-def get_disc_free():
-    return psutil.disk_usage("/").free
-
-
 def get_asset_actual_sum():
     #logger.debug(f'actual_asset_sum: {db.interaction.get_actual_asset_sum()}')
     return db.interaction.get_actual_asset_sum()
@@ -61,22 +42,6 @@ class CustomCollector(object):
         pass
 
     def collect(self):
-        cpu_usage = GaugeMetricFamily('system_cpu_percent_usage', '% использования cpu')
-        cpu_usage_metric = get_cpu_procent()
-        cpu_usage.add_metric([], cpu_usage_metric)
-
-        ram_usage = GaugeMetricFamily('system_ram_percent_usage', '% использования оперативы')
-        ram_usage_metric = get_virtual_memory_procent_usage()
-        ram_usage.add_metric([], ram_usage_metric)
-
-        disk_usage_percent = GaugeMetricFamily('system_disk_usage_percent', '% занятого места на диске')
-        disk_usage_metric = get_disc_usage_percent()
-        disk_usage_percent.add_metric([], disk_usage_metric)
-
-        disk_free_space = GaugeMetricFamily('system_disk_free_space', 'свободного места на диске')
-        disk_free_space_metric = get_disc_free()
-        disk_free_space.add_metric([], disk_free_space_metric)
-
         actual_asset_sum = GaugeMetricFamily('finance_actual_asset_sum', 'количество BTC')
         actual_asset_sum_metric = get_asset_actual_sum()
         actual_asset_sum.add_metric([], actual_asset_sum_metric)
